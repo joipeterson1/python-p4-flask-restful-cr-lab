9f9c8a0079de8231a3f562b1c4a2127fad6c1a51
Dalecia <petersondalecia@gmail.com>
2024-11-18 22:09:07 -0500

    done

--- server/app.py
+++ server/app.py
@@ -17,10 +17,52 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_to_dict = [p.to_dict() for p in Plant.query.all()]
+        response = make_response(
+            plants_to_dict,
+            200
+        )
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data.get('name'),
+            image=data.get('image'),
+            price=data.get('price')
+        )
+        db.session.add(new_plant)
+        db.session.commit()
 
+        response_dict = new_plant.to_dict()
+        response = make_response(
+            response_dict,
+            201
+        )
+        
+api.add_resource(Plants, '/plants')
+        
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        # Retrieve the plant by ID
+        plant = Plant.query.get(id)
+
+        # Check if the plant exists
+        if plant is None:
+            return make_response({'error': 'Plant not found'}, 404)
+
+        # Convert the plant object to a dictionary
+        plant_dict = plant.to_dict()
+
+        # Create a response with the plant data
+        response = make_response(
+            plant_dict,
+            200
+        )
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
